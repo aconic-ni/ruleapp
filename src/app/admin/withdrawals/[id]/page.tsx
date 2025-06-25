dea3adba86c6249e6258475192fe50bf28d6f8f9
aconic-ni <jerco27@gmail.com>
2025-06-25 17:15:12 -0600

    Delete src/app/admin/withdrawals/[id]/page.tsx

--- src/app/admin/withdrawals/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function WithdrawalDetailPage() {
-    // This page is no longer used and can be deleted.
-    // The withdrawal receipt is now shown in a dialog on the admin dashboard.
-    return null;
-}
